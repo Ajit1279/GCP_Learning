185124d6c14385834d3c9f8de517967cfa0d919a
Ajit1279 <81754034+Ajit1279@users.noreply.github.com>
2024-02-10 17:00:42 +0530

    Update index.js
    Added comments for decoding the pubsub message

--- 20240204_CldFunctions/Nodejs/nodejs_eventdriven/20240210_pubsub/index.js
+++ 20240204_CldFunctions/Nodejs/nodejs_eventdriven/20240210_pubsub/index.js
@@ -6,6 +6,8 @@ functions.cloudEvent('helloPubSub', cloudEvent => {
   // The Pub/Sub message is passed as the CloudEvent's data payload.
   const base64name = cloudEvent.data.message.data;
 
+// The Pub/Sub message's data is stored as a base64-encoded string in the data property of the PubsubMessage object. 
+// To extract this data, you may need to decode it as shown in the examples below.
   const name = base64name
     ? Buffer.from(base64name, 'base64').toString()
     : 'World';
