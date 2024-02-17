ca8a53454ea87cadb473def0860cb2edd9a9b451
Ajit1279 <81754034+Ajit1279@users.noreply.github.com>
2024-02-17 10:10:41 +0530

    Update main.py
    corrected main.py to resolve ) -> iam_policy _pb2.SetIamPolicy Request: NameError: name 'iam_policy_pb2' is not defined error

--- 20240214_Security_Identity/20240214_SecretManager/20240217_ManageAccess/Python/main.py
+++ 20240214_Security_Identity/20240214_SecretManager/20240217_ManageAccess/Python/main.py
@@ -1,31 +1,3 @@
-def iam_grant_access(
-    project_id: str, secret_id: str, member: str
-) -> iam_policy_pb2.SetIamPolicyRequest:
-    """
-    Grant the given member access to a secret.
-    """
-
-    # Import the Secret Manager client library.
-    from google.cloud import secretmanager
-
-    # Create the Secret Manager client.
-    client = secretmanager.SecretManagerServiceClient()
-
-    # Build the resource name of the secret.
-    name = client.secret_path(project_id, secret_id)
-
-    # Get the current IAM policy.
-    policy = client.get_iam_policy(request={"resource": name})
-
-    # Add the given member with access permissions.
-    policy.bindings.add(role="roles/secretmanager.secretAccessor", members=[member])
-
-    # Update the IAM Policy.
-    new_policy = client.set_iam_policy(request={"resource": name, "policy": policy})
-
-    # Print data about the secret.
-    print(f"Updated IAM policy on {secret_id}")
-
 # Import the Secret Manager client library.
 from google.cloud import secretmanager
 
@@ -33,7 +5,7 @@ from google.cloud import secretmanager
 project_id = "myprojec21"
 
 # ID of the secret to create.
-secret_id = "my-python-secret"
+secret_id = "create-python-secret"
 
 # Create the Secret Manager client.
 client = secretmanager.SecretManagerServiceClient()
