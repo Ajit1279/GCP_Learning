f86f4d09989e71c6ad014f69a6ce60e8a9d10ac2
Ajit1279 <81754034+Ajit1279@users.noreply.github.com>
2024-02-17 10:21:20 +0530

    Update main_manageaccess.py

--- 20240214_Security_Identity/20240214_SecretManager/20240217_ManageAccess/Python/main_manageaccess.py
+++ 20240214_Security_Identity/20240214_SecretManager/20240217_ManageAccess/Python/main_manageaccess.py
@@ -1,38 +1,27 @@
-# Import the Secret Manager client library.
-from google.cloud import secretmanager
+def iam_grant_access(
+    project_id: str, secret_id: str, member: str
+) -> iam_policy_pb2.SetIamPolicyRequest:
+    """
+    Grant the given member access to a secret.
+    """
 
-# GCP project in which to store secrets in Secret Manager.
-project_id = "myprojec21"
+    # Import the Secret Manager client library.
+    from google.cloud import secretmanager
 
-# ID of the secret to create.
-secret_id = "create-python-secret"
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
 
-# Create the Secret Manager client.
-client = secretmanager.SecretManagerServiceClient()
+    # Build the resource name of the secret.
+    name = client.secret_path(project_id, secret_id)
 
-# Build the parent name from the project.
-parent = f"projects/{project_id}"
+    # Get the current IAM policy.
+    policy = client.get_iam_policy(request={"resource": name})
 
-# Create the parent secret.
-secret = client.create_secret(
-    request={
-        "parent": parent,
-        "secret_id": secret_id,
-        "secret": {"replication": {"automatic": {}}},
-    }
-)
+    # Add the given member with access permissions.
+    policy.bindings.add(role="roles/secretmanager.secretAccessor", members=[member])
 
-# Add the secret version.
-version = client.add_secret_version(
-    request={"parent": secret.name, "payload": {"data": b"hello world!"}}
-)
+    # Update the IAM Policy.
+    new_policy = client.set_iam_policy(request={"resource": name, "policy": policy})
 
-# Access the secret version.
-response = client.access_secret_version(request={"name": version.name})
-
-# Print the secret payload.
-#
-# WARNING: Do not print the secret in a production environment - this
-# snippet is showing how to access the secret material.
-payload = response.payload.data.decode("UTF-8")
-print(f"Plaintext: {payload}")
+    # Print data about the secret.
+    print(f"Updated IAM policy on {secret_id}")
