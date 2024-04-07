8c39d7105a2221fa105f6190c565b37ea2db5a72
Ajit1279 <81754034+Ajit1279@users.noreply.github.com>
2024-04-07 15:57:15 +0530

    Update main.py
    Updated load table instructions

--- 20240316_BigDataAnalytics/20240325_BQ_DataLoad/20240406_CSVLoad_Python/main.py
+++ 20240316_BigDataAnalytics/20240325_BQ_DataLoad/20240406_CSVLoad_Python/main.py
@@ -1,10 +1,11 @@
+#reference: https://cloud.google.com/bigquery/docs/datasets#python
 from google.cloud import bigquery
 
 # Construct a BigQuery client object.
 client = bigquery.Client()
 
 # TODO(developer): Set dataset_id to the ID of the dataset to create.
-# dataset_id = "{}.your_dataset".format(client.project)
+dataset_id = "{}.mybqdataset".format(client.project)"
 
 # Construct a full Dataset object to send to the API.
 dataset = bigquery.Dataset(dataset_id)
@@ -17,3 +18,31 @@ dataset.location = "US"
 # exists within the project.
 dataset = client.create_dataset(dataset, timeout=30)  # Make an API request.
 print("Created dataset {}.{}".format(client.project, dataset.dataset_id))
+
+# Loading dataset into table: https://cloud.google.com/bigquery/docs/loading-data-cloud-storage-csv#loading_csv_data_into_a_table
+# https://cloud.google.com/bigquery/docs/loading-data-cloud-storage-csv#loading_csv_data_into_a_table
+
+# TODO(developer): Set table_id to the ID of the table to create.
+project = client.project
+table_name = mybqtable
+table_id = "project.dataset.table_name"
+
+job_config = bigquery.LoadJobConfig(
+    schema=[
+        bigquery.SchemaField("name", "STRING"),
+        bigquery.SchemaField("post_abbr", "STRING"),
+    ],
+    skip_leading_rows=1,
+    # The source format defaults to CSV, so the line below is optional.
+    source_format=bigquery.SourceFormat.CSV,
+)
+uri = "gs://cloud-samples-data/bigquery/us-states/us-states.csv"
+
+load_job = client.load_table_from_uri(
+    uri, table_id, job_config=job_config
+)  # Make an API request.
+
+load_job.result()  # Waits for the job to complete.
+
+destination_table = client.get_table(table_id)  # Make an API request.
+print("Loaded {} rows.".format(destination_table.num_rows))
