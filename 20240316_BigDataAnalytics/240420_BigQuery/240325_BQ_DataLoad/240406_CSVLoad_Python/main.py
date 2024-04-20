87126fb8673e8577fa512a1e8d6df237e5dccab2
Ajit1279 <81754034+Ajit1279@users.noreply.github.com>
2024-04-20 20:29:52 +0530

    Add files via upload

---
+++ 20240316_BigDataAnalytics/240420_BigQuery/240325_BQ_DataLoad/240406_CSVLoad_Python/main.py
@@ -0,0 +1,48 @@
+#reference: https://cloud.google.com/bigquery/docs/datasets#python
+from google.cloud import bigquery
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+# TODO(developer): Set dataset_id to the ID of the dataset to create.
+dataset_id = "{}.mybqdataset".format(client.project)
+
+# Construct a full Dataset object to send to the API.
+dataset = bigquery.Dataset(dataset_id)
+
+# TODO(developer): Specify the geographic location where the dataset should reside.
+dataset.location = "US"
+
+# Send the dataset to the API for creation, with an explicit timeout.
+# Raises google.api_core.exceptions.Conflict if the Dataset already
+# exists within the project.
+dataset = client.create_dataset(dataset, timeout=30)  # Make an API request.
+print("Created dataset {}.{}".format(client.project, dataset.dataset_id))
+
+# Loading dataset into table: https://cloud.google.com/bigquery/docs/loading-data-cloud-storage-csv#loading_csv_data_into_a_table
+# https://cloud.google.com/bigquery/docs/loading-data-cloud-storage-csv#loading_csv_data_into_a_table
+
+# TODO(developer): Set table_id to the ID of the table to create.
+#project = bigdata
+#table_name = mybqtable
+table_id = "bigdata0324.mybqdataset.mybqtable1"
+
+job_config = bigquery.LoadJobConfig(
+    schema=[
+        bigquery.SchemaField("name", "STRING"),
+        bigquery.SchemaField("post_abbr", "STRING"),
+    ],
+    skip_leading_rows=1,
+    # The source format defaults to CSV, so the line below is optional.
+    source_format=bigquery.SourceFormat.CSV,
+)
+uri = "gs://cloud-samples-data/bigquery/us-states/us-states.csv"
+
+load_job = client.load_table_from_uri(
+    uri, table_id, job_config=job_config
+)  # Make an API request.
+
+load_job.result()  # Waits for the job to complete.
+
+destination_table = client.get_table(table_id)  # Make an API request.
+print("Loaded {} rows.".format(destination_table.num_rows))
