0b1ad6f5289c74f39183fe76f644e2be719080de
Ajit1279 <81754034+Ajit1279@users.noreply.github.com>
2024-05-31 07:02:00 +0530

    Update BQPySpark.py

--- 20240316_BigDataAnalytics/240420_BigQuery/240519_Console/BQPySpark.py
+++ 20240316_BigDataAnalytics/240420_BigQuery/240519_Console/BQPySpark.py
@@ -4,21 +4,21 @@ OPTIONS(engine="SPARK", runtime_version="1.1")
 LANGUAGE PYTHON AS R"""
 from pyspark.sql import SparkSession
 
-      spark = SparkSession.builder.appName("spark-bigquery-demo").getOrCreate()
+spark = SparkSession.builder.appName("spark-bigquery-demo").getOrCreate()
 
-      # Load data from BigQuery.
-      words = spark.read.format("bigquery") \
-      .option("table", "bigquery-public-data:samples.shakespeare") \
-      .load()
-      words.createOrReplaceTempView("words")
+# Load data from BigQuery.
+words = spark.read.format("bigquery") \
+.option("table", "bigquery-public-data:samples.shakespeare") \
+.load()
+words.createOrReplaceTempView("words")
 
-      # Perform word count.
-      word_count = words.select('word', 'word_count').groupBy('word').sum('word_count').withColumnRenamed("sum(word_count)", "sum_word_count")
-      word_count.show()
-      word_count.printSchema()
+# Perform word count.
+word_count = words.select('word', 'word_count').groupBy('word').sum('word_count').withColumnRenamed("sum(word_count)", "sum_word_count")
+word_count.show()
+word_count.printSchema()
 
-      # Saving the data to BigQuery
-      word_count.write.format("bigquery") \
-      .option("writeMethod", "direct") \
-      .save("wordcount_dataset.wordcount_output")
-      """
+# Saving the data to BigQuery
+word_count.write.format("bigquery") \
+.option("writeMethod", "direct") \
+.save("wordcount_dataset.wordcount_output")
+"""
