078582eb2151f54f6bcccd17e2df655d38eac359
Ajit1279 <81754034+Ajit1279@users.noreply.github.com>
2025-01-03 15:27:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "my-bucket" {
   name                     = "apk-githubdemo-bucket"
   project                  = "DevOps"
-  location                 = "Asia"
+  location                 = "asia-south1-c"
   force_destroy            = "true"
   public_access_prevention = "enforced"
   }
