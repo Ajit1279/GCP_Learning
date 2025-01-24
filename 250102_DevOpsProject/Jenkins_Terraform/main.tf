d066e3893ad5756ba00ee8e68a947b82e18be832
Ajit1279 <81754034+Ajit1279@users.noreply.github.com>
2025-01-24 21:33:07 +0530

    Update main.tf

--- 250102_DevOpsProject/Jenkins_Terraform/main.tf
+++ 250102_DevOpsProject/Jenkins_Terraform/main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "my-bucket" {
   name                     = "apk-githubdemo-bucket"
-  project                  = "DevOps"
-  location                 = "Asia"
+  project                  = "devops1279"
+  location                 = "ASIA-SOUTH1"
   force_destroy            = "true"
   public_access_prevention = "enforced"
   }
